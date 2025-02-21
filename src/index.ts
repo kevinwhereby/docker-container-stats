be7852b32edfc737090141fe8bbc034d13ce511a
Kevin Hanna <kevin.hanna@whereby.com>
2025-02-21 11:48:32 +0000

    send json responses

--- src/index.ts
+++ src/index.ts
@@ -15,20 +15,20 @@ const containerMonitor = new ContainerMonitor();
 app.post("/monitor", async (req, res) => {
     const body = await req.body;
     if (!body.labels) {
-        res.status(400).send("labels property is required");
+        res.status(400).send(JSON.stringify({ message: "labels property is required" }));
         return;
     }
     if (typeof body.labels !== "object") {
-        res.status(400).send("labels property must be an object");
+        res.status(400).send(JSON.stringify({ message: "labels property must be an object" }));
         return;
     }
     const labels = body.labels;
     if (Object.keys(labels).length === 0) {
-        res.status(400).send("labels object must contain labels");
+        res.status(400).send(JSON.stringify({ message: "labels object must contain labels" }));
         return;
     }
     if (Object.values(labels).some((value) => typeof value !== "string" && typeof value !== null)) {
-        res.status(400).send("label values must be string or null");
+        res.status(400).send(JSON.stringify({ message: "label values must be string or null" }));
         return;
     }
 
@@ -39,7 +39,7 @@ app.post("/monitor", async (req, res) => {
         return;
     }
     if (results.length > 1) {
-        res.status(400).send("Found more than one container matching provided labels");
+        res.status(400).send(JSON.stringify({ message: "Found more than one container matching provided labels" }));
         return;
     }
 
@@ -48,19 +48,19 @@ app.post("/monitor", async (req, res) => {
 
     const container = docker.getContainer(containerDetails.Id);
     if (!container) {
-        res.status(404).send("Successfully listed details but could not fetch container");
+        res.status(404).send(JSON.stringify({ message: "Successfully listed details but could not fetch container" }));
         return;
     }
     console.log("Monitoring", container.id);
     containerMonitor.monitorContainer(container);
 
-    res.status(201).send(containerDetails.Id);
+    res.status(201).send(JSON.stringify({ containerId: containerDetails.Id }));
 });
 
 app.get("/monitor/:containerId", (req, res) => {
     const containerId = req.params.containerId;
     if (!containerId) {
-        res.status(400).send("container ID is required");
+        res.status(400).send(JSON.stringify({ message: "container ID is required" }));
         return;
     }
 
